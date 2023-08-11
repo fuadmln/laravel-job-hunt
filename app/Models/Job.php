aad719bf48d5e8226f8e111feeb6705da6a8d705
Fuad Maulana <fuadm03@gmail.com>
2023-08-11 22:10:16 +0700

    add: define model relationship

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Step;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
@@ -12,4 +13,9 @@ class Job extends Model
     protected $fillable = [
         'title', 'company', 'description', 'location', 'salary_min', 'salary_max', 'job_type', 'status', 'priority', 'note', 'reference'
     ];
+
+    public function steps()
+    {
+        return $this->hasMany(Step::class);
+    }
 }
--- app/Models/Step.php
+++ app/Models/Step.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Job;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Step extends Model
 {
@@ -12,4 +13,9 @@ class Step extends Model
     protected $fillable = [
         'job_id', 'name', 'description', 'start_date', 'due_date', 'schedule_date', 'status', 'note', 'review'
     ];
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
 }
