89b4de5737cf92b136da53a56fec1abcb22cae8d
Fuad Maulana <fuadm03@gmail.com>
2023-08-20 23:22:54 +0700

    feat: add index and show job routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('jobs', JobController::class)->only([
+    'index', 'show'
+]);
