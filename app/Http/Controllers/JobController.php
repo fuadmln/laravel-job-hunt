a509bde752915df8c312c32666fa325b6e272372
Fuad Maulana <fuadm03@gmail.com>
2023-08-21 16:26:23 +0700

    feat: add job controller with index and show methods

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index(): View
+    {
+        $jobs = Job::all()->toArray();
+        return view('jobs.index', compact('jobs'));
+    }
+
+    public function show(Job $job)
+    {
+        $job = $job->getAttributes();
+        return view('jobs.show', compact('job'));
+    }
+}
